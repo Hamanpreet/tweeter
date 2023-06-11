46d87842eb57e7ee962fb39edf372536a57d8f18
Hamanpreet <haman42626@gmail.com>
2023-06-11 15:48:47 -0400

    Counter red for extra characters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,28 @@
+/** 
+ * function runs a callback when DOM is ready to be manipulated with jQuery
+*/
+$(document).ready(function() {
+  let count = 0;
+  $("#tweet-text").on("input",function() {
+    //get the value of text-area
+    var inputValue = $(this).val();
+    var inputLength = inputValue.length;
+    //transverse up, find & update the counter with latest value 
+    $(this).closest('.new-tweet').find(".counter").text(140 - inputLength);
+    if (inputLength > 140) {
+      $(this).closest('.new-tweet').find(".counter").addClass("counter-red");
+    } else {
+      $(this).closest('.new-tweet').find(".counter").removeClass("counter-red");
+    }
+    
+  });
+});
+
+// //specify the DOM node to reference using the document.getElementById method and put that reference in a variable
+// const box = document.getElementById("tweet-text");
+
+// // when box is clicked, run the function
+// box.addEventListener("click", () => {
+//   console.log("You clicked on box.");
+// });
+
