30b19066b717fcc38b5aaaede059279af5a96adb
Hamanpreet <haman42626@gmail.com>
2023-06-14 17:48:37 -0400

    Render tweets from data array

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$(document).ready(function() {
 const data = [
   {
     "user": {
@@ -35,7 +35,22 @@ const data = [
  */
 const createTweetElement = function(tweet) {
   const $tweet = $(`
-  <article class="tweet">Hello World</article>
+  <article id="tweet">
+      <header class="tweet-header">
+        <p><i class="fa-solid fa-user"></i>${tweet.user.name}</p>
+        <p class="tweet-header-handle">${tweet.user.handle}</p>
+      </header>
+      <p class="tweet-text">${tweet.content.text}</p>
+      <hr>
+      <footer class="tweet-footer">
+        <p class="tweet-footer-info">${(tweet.created_at)}</p>
+        <div class="tweet-footer-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
   `);
   return $tweet;
 };
@@ -48,24 +63,34 @@ const renderTweets = function(tweets) {
   for(let tweet of tweets) {
     const value = createTweetElement(tweet);
     console.log(value);
-    $("#tweets-container").append(value);
+    $("#tweets-container").prepend(value);
   }
+};
+
+/**
+ * function to convert timestamp into days ago
+ */
+const timestamp = function(stamp) {
+  return moment().valueOf();
 }
+
+
 
 renderTweets(data);
 
 
-$(document).ready(function(event) {
-    $("#tweets-container").mouseenter(function() {
-      $(this).addClass("hover");
+
+    $("#tweet").on("mouseenter", function() {
+      $("#tweet").addClass("hover");
     });
-    $("#tweets-container").mouseleave(function() {
-      $(this).removeClass("hover");
+    $("#tweet").on("mouseleave", function() {
+      $("#tweet").removeClass("hover");
     });
-    $(".tweet-footer-icons i").mouseenter(function() {
+    $(".tweet-footer-icons i").on("mouseenter", function() {
+      //console.log(this);
       $(this).addClass("hovered");
     });
-    $(".tweet-footer-icons i").mouseleave(function() {
+    $(".tweet-footer-icons i").on("mouseleave", function() {
       $(this).removeClass("hovered");
     });
 });
