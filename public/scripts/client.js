d9b3b7d3dcd245f50531600e0183facb9928257c
Hamanpreet <haman42626@gmail.com>
2023-06-24 15:54:00 -0400

    Final commit before submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,16 +1,16 @@
-/** 
+/**
 * Client-side JS logic goes here
 * jQuery is already loaded
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-$(document).ready(function () {
+$(document).ready(function() {
 
   /**
   * Function to escape user text to prevent XSS attacks
   * @param string
   */
-  const escape = function (str) {
+  const escape = function(str) {
     //creates temporary div element
     let div = document.createElement("div");
     //appends a text node(text is treated as plain text not as HTML code)
@@ -21,10 +21,10 @@ $(document).ready(function () {
   
   /**
   * Using jQuery to construct new elements using $ function
-  * @param {object} tweets 
+  * @param {object} tweets
   */
-  const createTweetElement = function (tweet) {
-    const safeHTML = `${escape(tweet.content.text)}`
+  const createTweetElement = function(tweet) {
+    const safeHTML = `${escape(tweet.content.text)}`;
     const $tweet = $(`
     <article id="tweet">
         <header class="tweet-header">
@@ -52,7 +52,7 @@ $(document).ready(function () {
   /**
   * Taking an array of tweet objects & appending each to #tweets-container
   */
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
     $("#tweets-container").empty();
     for (const tweet of tweets) {
       const value = createTweetElement(tweet);
@@ -63,7 +63,7 @@ $(document).ready(function () {
   /**
    * responsible for fetching tweets from /tweets page
   */
-  const loadTweets = function () {
+  const loadTweets = function() {
     $.get("/tweets")
       .then(res => {
         renderTweets(res);
@@ -73,41 +73,47 @@ $(document).ready(function () {
       })
   };
 
+  //loads stored tweets upon starting server again
   loadTweets();
 
-  $("#tweet-text").on("input", function () {
+  $("#tweet-text").on("input", function() {
 
   });
 
-  $("#nav-button").on("click", function () {
+  //New in nav bar is configured as a button, opens up form
+  $("#nav-button").on("click", function() {
     $(".new-tweet").slideDown();
     $("#tweet-content").focus();  
-    $("#form-id").on("submit", function (e) {
-      e.preventDefault();
-      let inputLength = $("#tweet-content").val().length;
-      if (inputLength === 0) {
-        let errorMessage = "You cannot post an empty tweet.";
-        $("#error-message").text(errorMessage).slideDown("slow");
-        return;
-      } 
-      if (inputLength > 140) {
-        const errorMessage = "Tweet shouldn't have more than 140 characters.";
-        $("#error-message").text(errorMessage).slideDown("slow");
-        return;
-      } 
-        $("#error-message").slideUp("slow");
-        const data = $("#form-id").serialize();
-        $.post("/tweets", data)
-          .then(loadTweets)
-          .catch(err => {
-            console.log(err);
-          });
-      
-        $(".counter").text("140");
-        $("#tweet-content").val(" ");
-        $(".new-tweet").slideUp();
-
-    })
   });
+  
+  //posts the tweet if it is not empty and more than > 140 characters
+  $("#form-id").on("submit", function (e) {
+    e.preventDefault();
+    const inputLength = $("#tweet-content").val().length;
+    if (inputLength === 0) {
+      const errorMessage = "You cannot post an empty tweet.";
+      $("#error-message").text(errorMessage).slideDown("slow");
+      return;
+    }
+    if (inputLength > 140) {
+      const errorMessage = "Tweet shouldn't have more than 140 characters.";
+      $("#error-message").text(errorMessage).slideDown("slow");
+      return;
+    } else {
+      $("#error-message").slideUp("slow");
+      const data = $("#form-id").serialize();
+      $.post("/tweets", data)
+        .then(res => {
+          loadTweets();
+          document.getElementById("form-id").reset();
+          $(".counter").text("140");
+          $(".new-tweet").slideUp();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  });
+  
 });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * function runs a callback when DOM is ready to be manipulated with jQuery
 */
 
@@ -7,7 +7,7 @@ $(document).ready(function() {
     //get the value of text-area
     const inputValue = $(this).val();
     const inputLength = inputValue.length;
-    //transverse up, find & update the counter with latest value 
+    //transverse up, find & update the counter with latest value
     $(this).closest('.new-tweet').find(".counter").text(140 - inputLength);
     if (inputLength > 140) {
       $(this).closest('.new-tweet').find(".counter").addClass("counter-red");
@@ -17,6 +17,7 @@ $(document).ready(function() {
   });
 
 
+  //scroll-top button only appears after user has scrolled and nav button disappears(stretch)
   $(window).on( "scroll", function() {
     if ($(window).scrollTop() > 300) {
       $('.scroll-top').fadeIn('50');
@@ -27,11 +28,11 @@ $(document).ready(function() {
     }
   });
 
+  //scroll button to take us to top of the page and slide form down(stretch)
   $(".scroll-top").on ("click", () => {
     window.scroll(0,0);
     $(".new-tweet").slideDown();
     $("#tweet-content").focus();
-    $('')
   });
 
 });
